397e5cff359c79582d67ddb002a8c0aef4513ec9
Andres Loeh <andres@well-typed.com>
2021-01-18 17:06:53 +0100

    Add files for first part.

--- Lists.hs
+++ Lists.hs
@@ -4,4 +4,21 @@ module Lists where
 i42 :: Int
 i42 = 42
 
+{-@ measure size @-}
+{-@ size :: [a] -> Nat @-}
+size :: [a] -> Int
+size [] = 0
+size (x : xs) = 1 + size xs
+
+-- [] :: { r : [a] | size r == 0 }
+-- (:) :: a -> xs : [a] -> { r : [a] | size r == 1 + size xs }
+
+{-@ (!!!) :: xs : [a] -> { i : Nat | i < size xs } -> a @-}
+(!!!) :: [a] -> Int -> a
+(x : xs) !!! 0 = x
+(x : xs) !!! i = xs !!! (i - 1)
+_ !!! _ = error "impossible"
+
+-- error :: { s : String | false } -> a
+
 -- We will add contents to this module during the tutorial.
--- Memory.hs
+++ Memory.hs
@@ -1,3 +1,41 @@
 module Memory where
 
--- We will add contents to this module during the tutorial.
+import qualified Data.Set as S
+
+data Mem v = MEmp | MCons Int v (Mem v)
+
+{-@
+
+data Mem v =
+    MEmp
+  | MCons { addr :: Int, val :: v,  mem :: { m : Mem v | S.intersection (memAddrs m) (S.singleton addr) == S.empty } }
+
+@-}
+
+{-@ measure memAddrs @-}
+memAddrs :: Mem v -> S.Set Int
+memAddrs MEmp                 = S.empty
+memAddrs (MCons addr val mem) = S.union (S.singleton addr) (memAddrs mem)
+
+{-@ get :: addr : Int -> { mem : Mem v | S.member addr (memAddrs mem) } -> v @-}
+get :: Int -> Mem v -> v
+get addr (MCons addr' val' mem)
+  | addr == addr' = val'
+  | otherwise     = get addr mem
+get _ _ = error "impossible"
+
+{-@ set :: addr : Int -> v -> mem : Mem v
+   -> { r : Mem v | memAddrs r == S.union (S.singleton addr) (memAddrs mem) } @-}
+set :: Int -> v -> Mem v -> Mem v
+set addr val MEmp = MCons addr val MEmp
+set addr val (MCons addr' val' mem)
+  | addr == addr' = MCons addr val mem
+  | otherwise     = MCons addr' val' (set addr val mem)
+
+{-@ unset :: addr : Int -> mem : Mem v
+  -> { r : Mem v | memAddrs r == S.difference (memAddrs mem) (S.singleton addr) } @-}
+unset :: Int -> Mem v -> Mem v
+unset addr MEmp = MEmp
+unset addr (MCons addr' val' mem)
+  | addr == addr' = mem
+  | otherwise     = MCons addr' val' (unset addr mem)
